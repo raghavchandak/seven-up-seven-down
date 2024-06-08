e9a8b90a6dc4ab9e0fbd37acfd437713019161eb
raghavchandak <raghavchandak25@gmail.com>
2024-06-08 17:27:26 +0530

    Remade UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { calculateResult, updatePoints } from "./redux/gameSlice";
 import { rollDice } from "./api";
 import { isFulfilled } from "@reduxjs/toolkit";
-import Card from "@mui/joy/Card";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 // FOR DICE ICONS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -81,118 +81,155 @@ function App() {
     }
   };
 
+  // Creating more colours for mui
+  const { palette } = createTheme();
+  const { augmentColor } = palette;
+  const createColor = (mainColor) =>
+    augmentColor({ color: { main: mainColor } });
+  const theme = createTheme({
+    palette: {
+      gold: createColor("rgba(230,190,138, 0.8)"),
+      blue: createColor("#1A222B"),
+    },
+  });
+
   return (
-    <div className="App">
-      <Card variant="solid" className="card">
-        <h2>Current Points : {store.points}</h2>
-      </Card>
-      {result !== "" ? (
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            zIndex: "99",
-            padding: "10 20",
-            backgroundColor: "#1E2732",
-            borderRadius: "10px",
-            boxShadow: "10px 10px 5px rgba(0, 0, 0 , 0.3)",
-          }}
-        >
-          <h2 style={{ marginLeft: "1rem", marginRight: "1rem" }}>
-            {" "}
-            {result}{" "}
-          </h2>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <div className="top-bar">
+          <div style={{ display: "flex" }}>
+            <FontAwesomeIcon
+              icon="fa-solid fa-house"
+              style={{ marginRight: "0.5rem" }}
+            />
+            <FontAwesomeIcon icon="fa-solid fa-gear" />
+          </div>
+          <div className="points">Points : {store.points}</div>
         </div>
-      ) : null}
-      <div className="table">
-        <div className="betsContainer">
+        {result !== "" ? (
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
-              marginBottom: "2rem",
+              position: "absolute",
+              top: "50%",
+              zIndex: "99",
+              padding: "10 20",
+              backgroundColor: "#1E2732",
+              borderRadius: "10px",
+              boxShadow: "10px 10px 5px rgba(0, 0, 0 , 0.3)",
             }}
           >
-            <p style={{ fontWeight: "700", fontSize: "1rem" }}>
-              Choose Your Bet Amount:
-            </p>
-            <div
+            <h2 style={{ marginLeft: "1rem", marginRight: "1rem" }}>
+              {" "}
+              {result}{" "}
+            </h2>
+          </div>
+        ) : null}
+        <div className="table">
+          <div className="betsContainer">
+            <div className="bet-amount">
+              <p>PLACE YOUR BET</p>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  marginTop: 0,
+                  gap: "1rem",
+                  flexWrap: "wrap",
+                }}
+              >
+                <Button
+                  variant={betAmount === 100 ? "contained" : "outlined"}
+                  onClick={() => handleBetAmount(100)}
+                  className="betButton"
+                  color="gold"
+                >
+                  100
+                </Button>
+                <Button
+                  variant={betAmount === 200 ? "contained" : "outlined"}
+                  onClick={() => handleBetAmount(200)}
+                  className="betButton"
+                  color="gold"
+                >
+                  200
+                </Button>
+                <Button
+                  variant={betAmount === 500 ? "contained" : "outlined"}
+                  onClick={() => handleBetAmount(500)}
+                  className="betButton"
+                  color="gold"
+                >
+                  500
+                </Button>
+              </div>
+            </div>
+            <div className="bets">
+              <div
+                className="upper-box"
+                onClick={() => setBet(13)}
+                style={{ border: bet === 13 ? "1px solid #eccc9c" : null }}
+              >
+                <p>7 Up</p>
+              </div>
+              <div
+                className="circle"
+                onClick={() => setBet(7)}
+                style={{
+                  border: bet === 7 ? "5px solid #eccc9c" : "5px solid #11151f",
+                }}
+              >
+                <p>7</p>
+              </div>
+              <div
+                className="lower-box"
+                onClick={() => setBet(1)}
+                style={{ border: bet === 1 ? "1px solid #eccc9c" : null }}
+              >
+                <p>7 Down</p>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              height: "100%",
+              // backgroundColor: "red",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              variant="contained"
+              onClick={handleRoll}
+              disabled={bet === 0 || betAmount === 0 || error}
+              color="blue"
               style={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-                height: "10vh",
-                marginTop: 0,
-                gap: "1rem",
-                flexWrap: "wrap"
+                borderRadius: 50,
+                width: "10rem",
+                fontFamily: "Impact",
+                fontSize: "1rem",
+                letterSpacing: "2px",
               }}
             >
-              <Button
-                variant={betAmount === 100 ? "contained" : "outlined"}
-                onClick={() => handleBetAmount(100)}
-                className="betButton"
-              >
-                100
-              </Button>
-              <Button
-                variant={betAmount === 200 ? "contained" : "outlined"}
-                onClick={() => handleBetAmount(200)}
-                className="betButton"
-              >
-                200
-              </Button>
-              <Button
-                variant={betAmount === 500 ? "contained" : "outlined"}
-                onClick={() => handleBetAmount(500)}
-                className="betButton"
-              >
-                500
-              </Button>
-            </div>
-          </div>
-          <div className="bets">
-            <div className="upper-box" onClick={() => setBet(13)}>
-              <p>7 Up</p>
-            </div>
-            <div className="circle" onClick={() => setBet(7)}>
-              <p>7</p>
-            </div>
-            <div className="lower-box" onClick={() => setBet(1)}>
-              <p>7 Down</p>
-            </div>
-          </div>
-        </div>
-        <div
-          style={{
-            height: "100%",
-            // backgroundColor: "red",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-end",
-            alignItems: "center"
-          }}
-        >
-          <Button
-            variant="contained"
-            onClick={handleRoll}
-            disabled={bet === 0 || betAmount === 0 || error}
-            >
-            Roll!
-          </Button>
-          <div className="dice" id="dice">
-            <div className={diceClass} id="die1">
-              <FontAwesomeIcon icon={`fa-dice-${face1}`} className="icon" />
-            </div>
-            <div className={diceClass} id="die2">
-              <FontAwesomeIcon
-                icon={["fas", `fa-dice-${face2}`]}
-                className="icon"
-              />
+              Roll Dice
+            </Button>
+            <div className="dice" id="dice">
+              <div className={diceClass} id="die1">
+                <FontAwesomeIcon icon={`fa-dice-${face1}`} className="icon" />
+              </div>
+              <div className={diceClass} id="die2">
+                <FontAwesomeIcon
+                  icon={["fas", `fa-dice-${face2}`]}
+                  className="icon"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
